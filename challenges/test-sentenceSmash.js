1a424277b8b83f6438892e8c5d87b2f82579e9b6
Jon Rumsey <jon.rumsey@gmail.com>
2022-07-29 18:37:29 -0700

    added implementation and first test

--- challenges/test-sentenceSmash.js
+++ challenges/test-sentenceSmash.js
@@ -5,7 +5,7 @@ import sentenceSmash from "./sentenceSmash";
 describe('Testing challenge', () => {
   const inputs = [""];
 
-  test('It should print out an empty string', () => {
+  test('Empty string in empty string out', () => {
     expect(sentenceSmash(inputs)).toStrictEqual("");
     expect(fizzbuzz(inputs).length).toStrictEqual(0);
   });
