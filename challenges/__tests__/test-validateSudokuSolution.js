cff97b4509d72f18bea62885e7abeafa8f80631f
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-25 09:25:09 -0700

    implement a few simple tests.

---
+++ challenges/__tests__/test-validateSudokuSolution.js
@@ -0,0 +1,53 @@
+const { expect } = require("@jest/globals");
+const { default: test } = require("node:test");
+const validateSudokuSolution = require("../validate-sudoku-solution");
+
+describe("Test validator", () => {
+  const basicPassingPuzzle = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9.1, 2, 3, 4, 5, 6, 7],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+  ];
+
+  const basicFailingPuzzle = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9.1, 2, 3, 4, 5, 6, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+  ];
+
+  const zeroesFailingPuzzle = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 0, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9.1, 2, 3, 4, 5, 6, 7],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 0],
+  ];
+
+  test("basic valid puzzle returns true", () => {
+    expect(validateSudokuSolution(basicPassingPuzzle)).toBeTruthy();
+  });
+
+  test("basic invalid puzzle returns false", () => {
+    expect(validateSudokuSolution(basicFailingPuzzle)).toBeFalsy();
+  });
+
+  test("basic puzzle with zeros returns false", () => {
+    expect(validateSudokuSolution(zeroesFailingPuzzle)).toBeFalsy();
+  });
+});
