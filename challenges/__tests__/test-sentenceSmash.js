36f5ce74e24e6ab55aa2000aa5dd577589159c16
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-25 17:41:30 -0700

    added test, fixed existing.

--- challenges/__tests__/test-sentenceSmash.js
+++ challenges/__tests__/test-sentenceSmash.js
@@ -1,18 +1,31 @@
-"use strict";
-
 const { expect } = require("@jest/globals");
-// const { default: test } = require('node:test');
 const sentenceSmash = require("../sentenceSmash");
 
 describe("Testing challenge", () => {
-  const inputs = [""];
-
   test("Empty string in empty string out", () => {
+    const inputs = [""];
     expect(sentenceSmash.sentenceSmash(inputs)).toStrictEqual("");
     expect(sentenceSmash.sentenceSmash(inputs).length).toStrictEqual(0);
   });
 
   test("Null in empty string out", () => {
     expect(sentenceSmash.sentenceSmash(null)).toStrictEqual("");
+  });
+
+  test("The,fox,in,red,socks,jumps,over,the,rocks is returned as a full sentence", () => {
+    const input = [
+      "The",
+      "fox",
+      "in",
+      "red",
+      "socks",
+      "jumps",
+      "over",
+      "the",
+      "rocks",
+    ];
+    expect(sentenceSmash.sentenceSmash(input)).toStrictEqual(
+      "The fox in red socks jumps over the rocks"
+    );
   });
 });
