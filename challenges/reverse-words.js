cb3e2249d39ad90a9c23babfa2e276980c8d6ed4
Jon Rumsey <jon.rumsey@gmail.com>
2023-06-27 14:39:09 -0700

    added code comments.

--- challenges/reverse-words.js
+++ challenges/reverse-words.js
@@ -5,22 +5,35 @@
  * @returns {String} reversed sentance
  */
 module.exports = function reverseWords(str) {
+  // edge case: undefined input ? return empty string
   if (str === undefined) {
     return '';
   }
+
+  // split string into array of words built-in: O(n)
   const splitArr = str.split(' ');
   let resultStr = '';
+
+  // iterate over array and process each word: O(n)
   splitArr.forEach((item) => {
     let tempStr = '';
     // do not process existing spaces
     if (item === '' || item === ' ') {
       return;
     }
+    // iterate over word starting and end character and build a new string
+    // O(chars) + O(word)
     for (let idx = item.length - 1; idx >= 0; idx--) {
       const currentChar = item.charAt(idx);
       tempStr += currentChar;
     }
+    // expensively overwrite immutable object
     resultStr += tempStr + ' ';
   });
+
+  // remove any starting or training space
+  // characters before returning result
+  // overall Time O(words + charsInWords) -> O(n+m)
+  // Space O(n+1) -> O(n) where n is the number of words
   return resultStr.trim();
 };
